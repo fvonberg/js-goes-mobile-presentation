d9f29f176f0eb72bf0c259104fc98343ab1f200e
Frederik von Berg <vonberg@weiglewilczek.com>
2016-11-14 16:09:24 +0100

    update it-tage

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@
   /* @ngInject */
   function configureSlides(slidesConfig) {
     slidesConfig.slides = [
-      'contest',
-      'title',
+      'title-it-tage',
       'overview',
 
       '0-intro/about-us',
@@ -46,10 +45,9 @@
       '6-setup/nativescript-setup',
 
       '7-livecoding/live-coding-title',
-      '7-livecoding/live-coding-content',
+      // '7-livecoding/live-coding-content',
 
       'fragen',
-      'contest.end',
       'end',
       'quellen'
     ];
